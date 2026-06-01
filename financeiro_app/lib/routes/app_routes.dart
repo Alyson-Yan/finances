f9b529a7fa1653223566ec41658032d779d298ca
Alyson-Yan <alyson.cienciacomp@gmail.com>
2026-06-01 09:13:11 -0300

    Registra rota da visualização planilha

--- financeiro_app/lib/routes/app_routes.dart
+++ financeiro_app/lib/routes/app_routes.dart
@@ -1,6 +1,7 @@
 class AppRoutes {
   static const inicio = '/';
   static const lancamentos = '/lancamentos';
+  static const planilha = '/planilha';
   static const relatorioAnual = '/relatorio-anual';
   static const categorias = '/categorias';
   static const configuracoes = '/configuracoes';
